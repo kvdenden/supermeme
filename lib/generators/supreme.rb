81e893501586ab6427768ad15b8a8670522ebaba
Ken Van den Enden <ken@enden.be>
2018-11-18 17:40:04 -0800

    Set default options.

--- lib/generators/supreme.rb
+++ lib/generators/supreme.rb
@@ -1,7 +1,18 @@
 module Generators
   class Supreme
-    def self.call(text)
-      MagickTitle.say(text).url
+    DEFAULT_OPTIONS = {
+      color: "white",
+      background_color: "#da2727",
+      background_alpha: "ff",
+      font_size: 192,
+      font_path: "./lib/assets",
+      font: "Futura-Bold-Italic.ttf",
+      kerning: -10,
+      width: 2000
+    }
+
+    def self.call(text, **options)
+      MagickTitle.say(text, DEFAULT_OPTIONS.merge(options)).url
     end
   end
 end
