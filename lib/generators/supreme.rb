981ff3df9dfddaacc8e8249558142eab8c576011
Ken Van den Enden <ken@enden.be>
2018-11-23 09:25:27 -0800

    Tweak generated design.

--- lib/generators/supreme.rb
+++ lib/generators/supreme.rb
@@ -4,25 +4,29 @@ module Generators
 
     def self.call(text)
       # create image
-      image = Magick::Image.new(2000, 1000) { self.background_color = RED }
+      image = Magick::Image.new(8000, 1000) { self.background_color = RED }
 
       # create and set up draw object
       draw = Magick::Draw.new do
         self.gravity = Magick::CenterGravity
         self.fill = "white"
-        self.pointsize = 192
-        self.font = "./lib/assets/Futura-Bold-Italic.ttf"
+        self.pointsize = 720
+        self.font = "./lib/assets/Futura Heavy Italic.ttf"
       end
-      draw.kerning = -10
+      # reduce spacing between letters
+      draw.kerning = -36
 
       # draw text on image
       draw.annotate(image, 0, 0, 0, 0, text)
 
-      # trim image
+      # make image same size as text by removing whitespace
       image.trim!
 
-      # add border to image
-      image.border!(20, 10, RED)
+      # stretch image vertically
+      image.resize!(image.columns, image.rows * 1.1)
+
+      # add padding
+      image.border!(240, 80, RED)
 
       # write image to file
       image_path = Tempfile.new(['supermeme', '.png'], File.join(Dir.tmpdir, 'designs')).path
