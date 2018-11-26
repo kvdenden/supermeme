2154e8a21c7495d7b2ef489785b1521486238357
Ken Van den Enden <ken@enden.be>
2018-11-25 16:57:35 -0800

    Generate t-shirt mockups.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -11,8 +11,19 @@ class ImagesController < ApplicationController
     size = params.fetch("size", 100)
     image = generator.call(CGI::unescape(text), size: size.to_i)
 
-    respond_to do |format|
-      format.png  { send_data image.to_blob, type: image.mime_type, disposition: 'inline' }
-    end
+    send_data image.to_blob, type: image.mime_type, disposition: 'inline'
+  end
+
+  def mockup
+    designer = Designs::Supreme
+
+    variant = Variant.first # TODO: make this better :)
+    mockup_generator = Mockups::TShirt
+
+    text = params.fetch("text", "Supermeme")
+    design = designer.call(CGI::unescape(text), variant)
+    mockup = mockup_generator.call(design)
+
+    send_data mockup.to_blob, type: mockup.mime_type, disposition: 'inline'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'designs/new'
   get 'designs/show'
 
-  get 'images/:generator(/x:size.)*text.:format', to: 'images#show', as: :generated_image, defaults: { generator: 'supreme', format: "png" }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'images/mockups/*text.:format', to: 'images#mockup', as: :mockup_image, defaults: { format: 'jpg' }
+  get 'images/:generator(/x:size.)/*text.:format', to: 'images#show', as: :generated_image, defaults: { generator: 'supreme', format: 'png' }
 end
