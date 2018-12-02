01566a6da27687af6dbce263db68fe0a255d8c7c
Ken Van den Enden <ken@enden.be>
2018-12-02 12:01:57 +1100

    Generate printful payload to create an order.

---
+++ app/services/printful/create_order.rb
@@ -0,0 +1,42 @@
+module Printful
+  class CreateOrder
+    attr_reader :order
+
+    def initialize(order)
+      @order = order
+    end
+
+    def call
+      payload = purchase_order_payload
+      PrintfulAPI::Order.create(payload)
+    end
+
+    private
+
+    def purchase_order_payload
+      {
+        external_id: order.id,
+        recipient: {
+          name: order.address.recipient_name,
+          company: order.address.company_name,
+          address1: order.address.street1,
+          address2: order.address.street2,
+          city: order.address.city,
+          state_name: order.address.state,
+          country_code: order.address.country_code,
+        },
+        items: order.line_items.map { |item| line_item_payload(item) }
+      }
+    end
+
+    def line_item_payload(item)
+      {
+        variant_id: item.variant.external_id,
+        quantity: item.quantity,
+        files: [{
+          url: Rails.application.routes.url_helpers.product_variant_image_url(variant_id: item.variant_id, text: item.text)
+        }]
+      }
+    end
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module Supermeme
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    Rails.application.routes.default_url_options[:host] = ENV["DEFAULT_HOST"] || 'localhost:3000'
+    Rails.application.routes.default_url_options[:protocol] = ENV["DEFAULT_PROTOCOL"] || 'http'
 
     # Don't generate system test files.
     config.generators.system_tests = nil
