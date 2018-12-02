73ea3070e27be5c7721bacdc2025e929e64a3f38
Ken Van den Enden <ken@enden.be>
2018-12-02 15:53:27 +1100

    Clean up generated_image_path.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'designs/new'
   get 'designs/show'
 
-  get 'images/mockup/:design/*text.:format', to: 'images#mockup', as: :mockup_image, defaults: { design: 'supreme', format: 'jpg' }
+  get 'images/mockups/*text.:format', to: 'images#mockup', as: :mockup_image, defaults: { format: 'jpg' }
   get 'images/:variant_id/:design/*text.:format', to: 'images#product_variant', as: :product_variant_image, defaults: { design: 'supreme', format: 'png' }
-  get 'images/:design(/x:size.)/*text.:format', to: 'images#show', as: :generated_image, defaults: { design: 'supreme', format: 'png' }
+  get 'images/:generator(/x:size)/*text.:format', to: 'images#show', as: :generated_image, defaults: { generator: 'supreme', format: 'png' }
 end
