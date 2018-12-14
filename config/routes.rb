942784ccddbecc11d0bff2c8dd7b83bb4e969988
Ken Van den Enden <ken@enden.be>
2018-12-14 15:46:27 +0900

    Create checkout controller.

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get 'images/mockups/*text.:format', to: 'images#mockup', as: :mockup_image, defaults: { design: 'supreme', format: 'jpg' }
   get 'images/printfiles/:variant_id/:design/*text.:format', to: 'images#product_variant', as: :product_variant_image, defaults: { design: 'supreme', format: 'png' }
   get 'images/:design(/x:size)/*text.:format', to: 'images#show', as: :generated_image, defaults: { design: 'supreme', format: 'png' }
+
+  post 'checkout', to: 'checkout#create', as: :checkout
 end
