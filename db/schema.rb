6c6950019401870ba4053071e3de70149e7c7bda
Ken Van den Enden <ken@enden.be>
2018-12-14 19:37:22 +0900

    Remove name from addresses.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_101240) do
+ActiveRecord::Schema.define(version: 2018_12_14_103402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,6 @@ ActiveRecord::Schema.define(version: 2018_12_14_101240) do
     t.string "state"
     t.string "zip_code"
     t.string "country_code"
-    t.string "recipient_name"
-    t.string "company_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
