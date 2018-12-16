be005aaf625c12918dde0c74c4a0ee9d8b547a0f
Ken Van den Enden <ken@enden.be>
2018-12-16 22:22:35 +0900

    Handle empty stripe credentials in initializer.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,5 @@
-Rails.configuration.stripe = Rails.application.credentials[Rails.env.to_sym][:stripe]
+Rails.configuration.stripe = Rails.application.credentials.dig(Rails.env.to_sym, :stripe)
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+if Rails.configuration.stripe
+  Stripe.api_key = Rails.configuration.stripe[:secret_key]
+end
