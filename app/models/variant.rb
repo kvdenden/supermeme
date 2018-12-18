47f6a5be24db723e2b451c50f95407f109217ce8
Ken Van den Enden <ken@enden.be>
2018-12-18 16:40:55 +0900

    Add validation for products, variants and printfiles.

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -1,4 +1,9 @@
 class Variant < ApplicationRecord
   belongs_to :product
   belongs_to :printfile
+
+  validates :external_id, presence: true
+  validates :color, presence: true
+  validates :size, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
