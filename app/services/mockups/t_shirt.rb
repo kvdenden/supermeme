3c46800b643c90075a8514f825b956075550e067
Ken Van den Enden <ken@enden.be>
2018-12-21 11:02:47 +0900

    Cache images.

--- app/services/mockups/t_shirt.rb
+++ app/services/mockups/t_shirt.rb
@@ -1,17 +1,19 @@
 module Mockups
   class TShirt
     def self.call(design, max_width: 1200, max_height: 1200)
-      mockup_file = File.open('./lib/assets/mockups/tshirt-black.jpg')
-      mockup = Magick::Image.read(mockup_file).first
+      Rails.cache.fetch("mockups/supreme/#{design.hash}/#{max_width}x#{max_height}") do
+        mockup_file = File.open('./lib/assets/mockups/tshirt-black.jpg')
+        mockup = Magick::Image.read(mockup_file).first
 
-      canvas_width = 300
-      canvas_height = 400
-      resized_design = design.resize_to_fit(canvas_width, canvas_height)
+        canvas_width = 300
+        canvas_height = 400
+        resized_design = design.resize_to_fit(canvas_width, canvas_height)
 
-      offset_y = -30
-      mockup.composite!(resized_design, Magick::CenterGravity, 0, offset_y, Magick::OverCompositeOp)
+        offset_y = -30
+        mockup.composite!(resized_design, Magick::CenterGravity, 0, offset_y, Magick::OverCompositeOp)
 
-      mockup.resize_to_fit(max_width, max_height).sharpen(0)
+        mockup.resize_to_fit(max_width, max_height).sharpen(0)
+      end
     end
   end
 end
