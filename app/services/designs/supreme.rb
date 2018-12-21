a2a154001ed79ddf02143c4af31e930ab91fcd98
Ken Van den Enden <ken@enden.be>
2018-12-21 12:10:13 +0900

    Remove cache for designs and mockups.

--- app/services/designs/supreme.rb
+++ app/services/designs/supreme.rb
@@ -1,30 +1,28 @@
 module Designs
   class Supreme
     def self.call(text, variant)
-      Rails.cache.fetch("designs/supreme/#{text}/#{variant.id}") do
-        # get dimensions of printfile
-        canvas_width = variant.printfile.width
-        canvas_height = variant.printfile.height
+      # get dimensions of printfile
+      canvas_width = variant.printfile.width
+      canvas_height = variant.printfile.height
 
-        canvas = Magick::Image.new(canvas_width, canvas_height) do |image|
-          image.background_color = 'Transparent'
-          image.format = 'PNG'
-        end
-
-        # calculate suitable font size
-        font_size = canvas_width * 0.15
-        design = generator.call(text, size: font_size)
-
-        # scale down design if it is too big
-        max_width = canvas_width * 0.8
-        if design.columns > max_width
-          design.resize!(max_width / design.columns)
-        end
-
-        # place design on canvas
-        offset_y = canvas_height * -0.25
-        canvas.composite!(design, Magick::CenterGravity, 0, offset_y, Magick::OverCompositeOp)
+      canvas = Magick::Image.new(canvas_width, canvas_height) do |image|
+        image.background_color = 'Transparent'
+        image.format = 'PNG'
       end
+
+      # calculate suitable font size
+      font_size = canvas_width * 0.15
+      design = generator.call(text, size: font_size)
+
+      # scale down design if it is too big
+      max_width = canvas_width * 0.8
+      if design.columns > max_width
+        design.resize!(max_width / design.columns)
+      end
+
+      # place design on canvas
+      offset_y = canvas_height * -0.25
+      canvas.composite!(design, Magick::CenterGravity, 0, offset_y, Magick::OverCompositeOp)
     end
 
     def self.generator
--- app/services/mockups/t_shirt.rb
+++ app/services/mockups/t_shirt.rb
@@ -1,20 +1,18 @@
 module Mockups
   class TShirt
     def self.call(design, max_width: 1200, max_height: 1200)
-      Rails.cache.fetch("mockups/supreme/#{design.hash}/#{max_width}x#{max_height}") do
-        mockup_file_path = File.join(__dir__, "assets", "tshirt-black.jpg")
-        mockup_file = File.open(mockup_file_path)
-        mockup = Magick::Image.read(mockup_file).first
+      mockup_file_path = File.join(__dir__, "assets", "tshirt-black.jpg")
+      mockup_file = File.open(mockup_file_path)
+      mockup = Magick::Image.read(mockup_file).first
 
-        canvas_width = 300
-        canvas_height = 400
-        resized_design = design.resize_to_fit(canvas_width, canvas_height)
+      canvas_width = 300
+      canvas_height = 400
+      resized_design = design.resize_to_fit(canvas_width, canvas_height)
 
-        offset_y = -30
-        mockup.composite!(resized_design, Magick::CenterGravity, 0, offset_y, Magick::OverCompositeOp)
+      offset_y = -30
+      mockup.composite!(resized_design, Magick::CenterGravity, 0, offset_y, Magick::OverCompositeOp)
 
-        mockup.resize_to_fit(max_width, max_height).sharpen(0)
-      end
+      mockup.resize_to_fit(max_width, max_height).sharpen(0)
     end
   end
 end
