36c6558b10c0d7d0ebf59201a2746d180fb83a62
Ken Van den Enden <ken@enden.be>
2018-12-23 15:13:19 +0900

    Remove placeholder text from footer.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def copyright_message
+    from_year = 2018
+    current_year = Date.current.year
+
+    display_year = current_year == from_year ? "#{from_year}" : "#{from_year}-#{current_year}"
+    "&copy; #{display_year} Supermeme. All Rights Reserved."
+  end
 end
