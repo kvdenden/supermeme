10db9b045b3d8afc2654bd7588b9b5140ac86b41
Ken Van den Enden <ken@enden.be>
2018-12-31 20:30:13 +0900

    Calculate image size and padding.

--- app/services/generators/supreme.rb
+++ app/services/generators/supreme.rb
@@ -5,11 +5,6 @@ module Generators
 
     def self.call(text, size: BASE_SIZE)
       Rails.cache.fetch("generators/supreme/#{text}/#{size}") do
-        # create image
-        image = Magick::Image.new(8000, 1000) do |image|
-          image.background_color = RED
-          image.format = 'PNG'
-        end
 
         # create and set up draw object
         draw = Magick::Draw.new do
@@ -21,17 +16,37 @@ module Generators
         # reduce spacing between letters
         draw.kerning = -36
 
+        # calculate image dimensions
+        type_metrics = draw.get_type_metrics(text)
+
+        image_width = type_metrics.width + 100
+        image_height = type_metrics.height + 100
+
+        # create image
+        image = Magick::Image.new(image_width, image_height) do |image|
+          image.background_color = RED
+          image.format = 'PNG'
+        end
+
         # draw text on image
         draw.annotate(image, 0, 0, 0, 0, text)
 
         # make image same size as text by removing whitespace
         image.trim!
 
-        # stretch image vertically
-        image.resize!(image.columns, image.rows * 1.1)
+        # calculate padding
+        min_width = 2000
+        min_height = 800
+        min_horizontal_padding = 250
+        min_vertical_padding = 75
+        horizontal_padding = [(min_width - image.columns) / 2, min_horizontal_padding].max
+        vertical_padding = [(min_height - image.rows) / 2, min_vertical_padding].max
 
         # add padding
-        image.border!(240, 80, RED)
+        image.border!(horizontal_padding, vertical_padding, RED)
+
+        # stretch image vertically
+        image.resize!(image.columns, image.rows * 1.1)
 
         if size != BASE_SIZE
           scale_factor = size / BASE_SIZE.to_f
