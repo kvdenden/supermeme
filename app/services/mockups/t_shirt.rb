4c80c488b858a4271f8ab6ab90d04939eaceb010
Ken Van den Enden <ken@enden.be>
2019-01-01 21:28:49 +0900

    Add mockup for white t-shirt.

--- app/services/mockups/t_shirt.rb
+++ app/services/mockups/t_shirt.rb
@@ -1,16 +1,42 @@
 module Mockups
   class TShirt
-    def self.call(design, max_width: 1200, max_height: 1200)
-      mockup_file_path = File.join(__dir__, "assets", "tshirt-black.jpg")
-      mockup_file = File.open(mockup_file_path)
-      mockup = Magick::Image.read(mockup_file).first
+    COLOR_CONFIG = {
+      'Black' => {
+        filename: "tshirt-black.jpg",
+        offset_x: 0,
+        offset_y: -30,
+        rotation: -2
+      },
+      'White' => {
+        filename: "tshirt-white.jpg",
+        offset_x: 15,
+        offset_y: -30,
+        rotation: 8
+      }
+    }
+
+    def initialize(color: 'Black')
+      config = COLOR_CONFIG[color]
+      raise ArgumentError.new("Unsupported Color") if config.nil?
+
+      @filename = config[:filename]
+      @offset_x = config[:offset_x]
+      @offset_y = config[:offset_y]
+      @rotation = config[:rotation]
+    end
+
+    def call(design, max_width: 1200, max_height: 1200)
+      product_file_path = File.join(__dir__, "assets", @filename)
+      product_file = File.open(product_file_path)
+      product = Magick::Image.read(product_file).first
 
       canvas_width = 300
       canvas_height = 400
       resized_design = design.resize_to_fit(canvas_width, canvas_height)
 
-      offset_y = -30
-      mockup.composite!(resized_design, Magick::CenterGravity, 0, offset_y, Magick::OverCompositeOp)
+      resized_design.rotate!(@rotation) unless @rotation.zero?
+
+      mockup = product.composite!(resized_design, Magick::CenterGravity, @offset_x, @offset_y, Magick::OverCompositeOp)
 
       mockup.resize_to_fit(max_width, max_height).sharpen(0)
     end
