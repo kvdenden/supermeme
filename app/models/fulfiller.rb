a604307ce7ca494ab3b08bab505560406cf43743
Ken Van den Enden <ken@enden.be>
2019-01-12 12:39:03 +0900

    Create Fulfiller model.

---
+++ app/models/fulfiller.rb
@@ -0,0 +1,2 @@
+class Fulfiller < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_21_065010) do
+ActiveRecord::Schema.define(version: 2019_01_12_033554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2018_12_21_065010) do
     t.string "state_code"
     t.string "zip_code"
     t.string "country_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fulfillers", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
